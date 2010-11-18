85054f8f5bd369f2b179bbf92ad4914da4428b64
juanchi@juanchi.com.ar <juanchi@juanchi.com.ar@f98aa0b5-7540-5fc6-d165-0b43fa6fa215>
2010-11-18 22:30:36 +0000

    Added support for resuming downloads, uploading missing class definition Range.cs

---
+++ trunk/Topeka/Range.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topeka
+{
+    /// <summary>
+    /// A Class that represents a Range
+    /// </summary>
+    internal class Range
+    {
+        internal long _firstByte;
+        /// <summary>
+        /// The First Byte of the Range
+        /// </summary>
+        public long FirstByte
+        { get { return _firstByte; } }
+
+        internal long _lastByte;
+        /// <summary>
+        /// The Last Byte of the Range
+        /// </summary>
+        public long LastByte
+        { get { return _lastByte; } }
+
+        internal Range(long firstByte, long lastByte)
+        { 
+            this._firstByte = firstByte;
+            this._lastByte = lastByte;
+        }
+
+        internal Range(string input)
+        {
+            this._firstByte = 0;
+            this._lastByte = 0;
+
+            Tokenizer values = new Tokenizer(input, "-");
+            try { this._firstByte = (long)long.Parse(values.nextToken()); }
+            catch (Exception) { }
+            try { this._lastByte = (long)long.Parse(values.nextToken()); }
+            catch (Exception) { }
+        }
+
+    }
+}
