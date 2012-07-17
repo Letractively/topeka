085dfa22b587c36a0ec03145d05845dbeca462bf
juanchi <juanchi@juanchi.com.ar>
2012-07-17 22:47:39 +0000

    Added support for static pages read from an Assembly

---
+++ TestServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Topeka;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace TestServer
+{
+    class Program : Servlet
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server(2000);
+            server.setAssembly(Assembly.GetExecutingAssembly(), "TestServer");
+            server.start();
+            Process.Start("http://localhost:2000");
+        }
+
+        public override void doGet(Request request, Response response)
+        {
+            response.println("Hello TestServer");
+        }
+    }
+}
